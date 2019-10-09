699034115c01186a22d951cf9ede6d73ab76acb9
Daniel Hedrén <danielhedren@gmail.com>
2019-10-09 09:39:10 +0200

    Update v1.php

--- public/api/v1.php
+++ public/api/v1.php
@@ -3,17 +3,17 @@
 require_once $_SERVER['DOCUMENT_ROOT']."/../src/database.php";
 require_once $_SERVER['DOCUMENT_ROOT']."../../src/DAO/User.php";
 
-$json = json_decode($_POST["data"]);
+$json = json_decode(file_get_contents("php://input"));
 
-if ($json["function"] == "fetch_user") {
-    if (filter_var($json["user_id"], FILTER_VALIDATE_INT)) {
-        echo json_encode(User::fetch($json["user_id"]));
+if ($json->function == "fetch_user") {
+    if (filter_var($json->user_id, FILTER_VALIDATE_INT)) {
+        echo json_encode(User::fetch($json->user_id));
     }
-} else if ($json["function"] == "create_user") {
-    $email = filter_var($json["emaiL"], FILTER_VALIDATE_EMAIL);
-} else if ($json["function"] == "verify_user") {
-    $email = filter_var($json["email"], FILTER_VALIDATE_EMAIL);
-    $password = $json["password"];
+} else if ($json->function == "create_user") {
+    $email = filter_var($json->emaiL, FILTER_VALIDATE_EMAIL);
+} else if ($json->function == "verify_user") {
+    $email = filter_var($json->email, FILTER_VALIDATE_EMAIL);
+    $password = $json->password;
 
     if ($email && $password) {
         $user = User::fetch_by_email($email);
