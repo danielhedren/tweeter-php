08493cb2161d8c6ec62f0e85222caa49354098ec
Daniel Hedrén <danielhedren@gmail.com>
2019-10-09 09:24:06 +0200

    Begin user api

---
+++ public/api/v1.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once "../src/database.php";
+require_once "../src/DAO/User.php";
+
+$json = json_decode($_POST["data"]);
+
+if ($json["function"] == "fetch_user") {
+    if (filter_var($json["user_id"], FILTER_VALIDATE_INT)) {
+        echo json_encode(User::fetch($json["user_id"]));
+    }
+} else if ($json["function"] == "create_user") {
+    $email = filter_var($json["emaiL"], FILTER_VALIDATE_EMAIL);
+} else if ($json["function"] == "verify_user") {
+    $email = filter_var($json["email"], FILTER_VALIDATE_EMAIL);
+    $password = $json["password"];
+
+    if ($email && $password) {
+        $user = User::fetch_by_email($email);
+        if ($user && $user->verify($password)) {
+            echo json_encode(["status" => true]);
+        } else {
+            echo json_encode(["status" => false]);
+        }
+    } else {
+        echo json_encode(["status" => false]);
+    }
+}
--- src/DAO/User.php
+++ src/DAO/User.php
@@ -4,17 +4,55 @@ require_once "../src/database.php";
 
 class User
 {
-    public $id;
+    private $id;
     public $email;
     public $displayname;
-    public $password;
+    private $password;
     public $date;
 
-    public static function fetch($user_id) {
+    public static function fetch($user_id): User
+    {
         $stmt = Database::get_pdo()->prepare("SELECT * FROM User WHERE id = :id;");
         $stmt->bindParam(":id", $user_id, PDO::PARAM_INT);
         $stmt->execute();
         $user = $stmt->fetchObject(self::class);
         return $user;
+    }
+
+    public static function fetch_by_email($email): User
+    {
+        $stmt = Database::get_pdo()->prepare("SELECT * FROM User WHERE email = :email;");
+        $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+        $stmt->execute();
+        $user = $stmt->fetchObject(self::class);
+        return $user;
+    }
+
+    public function save()
+    {
+        if (isset($this->id)) {
+            $stmt = Database::get_pdo()->prepare("UPDATE User SET email = :email, displayname = :displayname, password = :password WHERE id = :id);");
+            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        } else {
+            $stmt = Database::get_pdo()->prepare("INSERT INTO User (email, displayname, password) VALUES (:email, :displayname, :password);");
+        }
+        $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+        $stmt->bindParam(":displayname", $displayname, PDO::PARAM_STR);
+        $stmt->bindParam(":password", $password, PDO::PARAM_STR);
+
+        // TODO: Handle unique constraint exceptions
+        $stmt->execute();
+    }
+
+    public function set_password($password) {
+        if (count($password) < 8) return false;
+
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+        return true;
+    }
+
+    public function verify($password)
+    {
+        return password_verify($password, $this->password);
     }
 }
