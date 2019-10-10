721b242f501a6384d20bd669fa62e07be9f04d53
Daniel Hedrén <danielhedren@gmail.com>
2019-10-10 10:28:17 +0200

    Frontend work, navbar + cards

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once $_SERVER['DOCUMENT_ROOT']."../../src/DAO/Comment.php";
+require_once $_SERVER['DOCUMENT_ROOT']."../../src/DAO/Comment.php";
 ?>
 
 <!doctype html>
@@ -12,9 +12,31 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Tweeter</title>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Tweeter</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        </ul>
+        <?php
+        if (!isset($_SESSION["userid"])) {
+            echo '<form id="loginForm" class="form-inline my-2 my-lg-0">';
+            echo '    <input id="loginEmail" class="form-control mr-sm-2" type="email" placeholder="Email" aria-label="Email">';
+            echo '    <input id="loginPassword" class="form-control mr-sm-2" type="password" placeholder="Password" aria-label="Password">';
+            echo '    <button id="loginButton" class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>';
+            echo '</form>';
+        }
+        ?>
+    </div>
+</nav>
+
 <div class="container">
     <h1>Hello, world!</h1>
 
@@ -22,7 +44,8 @@
     $comments = Comment::fetch_chronological(10, 0);
 
     foreach ($comments as $c) {
-        echo "<span>".$c->content."</span>";
+
+        echo '<div class="card"><div class="card-body">'.$c->content.'</div></div>';
     }
     ?>
 </div>
@@ -32,5 +55,9 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>
+    "use strict;"
+
+</script>
 </body>
 </html>
