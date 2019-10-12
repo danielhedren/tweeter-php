27fcd72f4ec4698f13ee5f5a08650ff1178f6071
Daniel Hedrén <danielhedren@gmail.com>
2019-10-12 10:57:40 +0200

    Use global namespace where required

--- src/DAO/DisplayComment.php
+++ src/DAO/DisplayComment.php
@@ -20,7 +20,7 @@ class DisplayComment
 
     public static function fetch($id) {
         $stmt = Database::get_pdo()->prepare("SELECT c.*, u.displayname FROM Comment c JOIN User u ON c.user_id = u.id WHERE id = :id;");
-        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->bindParam(":id", $id, \PDO::PARAM_INT);
         $stmt->execute();
         $comment = $stmt->fetchObject(self::class);
         if (!comment) return null;
@@ -31,11 +31,11 @@ class DisplayComment
     public static function fetch_chronological($num, $page)
     {
         $stmt = Database::get_pdo()->prepare("SELECT c.*, u.displayname FROM Comment c JOIN User u ON c.user_id = u.id ORDER BY c.date DESC LIMIT :limit OFFSET :offset");
-        $stmt->bindParam(":limit", $num, PDO::PARAM_INT);
-        $stmt->bindValue(":offset", $page * $num, PDO::PARAM_INT);
+        $stmt->bindParam(":limit", $num, \PDO::PARAM_INT);
+        $stmt->bindValue(":offset", $page * $num, \PDO::PARAM_INT);
         $stmt->execute();
 
-        $result = $stmt->fetchAll(PDO::FETCH_CLASS, "DisplayComment");
+        $result = $stmt->fetchAll(\PDO::FETCH_CLASS, "DisplayComment");
 
         return $result;
     }
