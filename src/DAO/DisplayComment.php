3362c2755e9d3331c333149eab9db6153b3ab028
Daniel Hedrén <danielhedren@gmail.com>
2019-10-15 19:30:23 +0200

    Add backend support for votes

--- src/DAO/DisplayComment.php
+++ src/DAO/DisplayComment.php
@@ -12,6 +12,7 @@ class DisplayComment
     public ?int $parent_id;
     public ?string $content;
     public string $displayname;
+    public int $votes;
 
     public function __construct()
     {
@@ -26,7 +27,7 @@ class DisplayComment
 
     public static function fetch($id): ?DisplayComment
     {
-        $stmt = Database::get_pdo()->prepare("SELECT c.*, u.displayname FROM Comment c JOIN User u ON c.user_id = u.id WHERE c.id = :id;");
+        $stmt = Database::get_pdo()->prepare("SELECT c.*, u.displayname, (SELECT COUNT(*) FROM VoteTernary v WHERE v.comment_id = c.id) as votes FROM Comment c JOIN User u ON c.user_id = u.id WHERE c.id = :id;");
         $stmt->bindParam(":id", $id, PDO::PARAM_INT);
         $stmt->execute();
         $comment = $stmt->fetchObject(self::class);
@@ -37,7 +38,7 @@ class DisplayComment
 
     public static function fetch_chronological($num, $page): array
     {
-        $stmt = Database::get_pdo()->prepare("SELECT c.*, u.displayname FROM Comment c JOIN User u ON c.user_id = u.id ORDER BY c.date DESC LIMIT :limit OFFSET :offset");
+        $stmt = Database::get_pdo()->prepare("SELECT c.*, u.displayname, (SELECT COUNT(*) FROM VoteTernary v WHERE v.comment_id = c.id) as votes FROM Comment c JOIN User u ON c.user_id = u.id ORDER BY c.date DESC LIMIT :limit OFFSET :offset");
         $stmt->bindParam(":limit", $num, PDO::PARAM_INT);
         $stmt->bindValue(":offset", $page * $num, PDO::PARAM_INT);
         $stmt->execute();
